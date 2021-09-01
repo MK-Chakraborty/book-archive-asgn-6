b085f4b1b82ed2bb12a2a3401c6d9c057737ac31
Manabendra <mkchakraborty5@gmail.com>
2021-09-02 02:49:52 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,45 @@
+const bookContainer = document.getElementById('bookContainer');
+
+// Fetching Data from api
+document.getElementById('searchBtn').addEventListener('click', () => {
+    bookContainer.textContent = '';
+    const searchedItem = document.getElementById('searchBox').value;
+
+    // if search box is empty
+    if (searchedItem === '') {
+        document.getElementById('totalBook').innerText = 'Search Field can\'t be empty.';
+        return;
+    }
+
+    const url = `https://openlibrary.org/search.json?q=${searchedItem}`;
+    document.getElementById('totalBook').innerText = '';
+    fetch(url)
+        .then(response => response.json())
+        .then(data => displayBook(data));
+});
+
+
+
+// Displaying data
+
+const displayBook = books => {
+    document.getElementById('searchBox').value = '';
+    document.getElementById('totalBook').innerText = `${books.numFound} Books Found`;
+    books.docs.forEach(book => {
+
+        // placeholder image when cover_i isn't available
+        if (!book.cover_i) { book.cover_i = 9289367; }
+
+        bookContainer.innerHTML += `
+            <div class="card my-5" style="width: 24rem;">
+                <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h3 class="card-title fs-3 fw-bold">${book.title}</h3>
+                    <h5 class="card-text fs-5">Authors: ${book.author_name ? book.author_name : 'Unavailable'}</h5>
+                    <h5 class="card-text fs-5">Publish year: ${book.first_publish_year ? book.first_publish_year : 'Unavailable'}</h5>
+                </div>
+            </div>
+    `;
+    });
+
+}
